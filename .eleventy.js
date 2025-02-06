290229b61f6b2764f946f05ed4d4d498f1c0f599
Vaiva Staugaityte <91862082+vaivatuta@users.noreply.github.com>
2025-02-06 19:47:55 +0100

    showing up as list

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/js");
+  
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByTag("posts").reverse();
+  });
 
   return {
     passthroughFileCopy: true,
